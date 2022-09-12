e291bede62ad03468b9c3052e1c90b02a752b81a
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-09-12 12:56:08 -0700

    Add 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints all possible
+ * different unique combinations of three digits
+ * Using the putchar function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, o, u;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		for (o = i + 1; o <= '9'; o++)
+		{
+			for (u = o + 1; u <= '9'; u++)
+			{
+				if ((o != i) && (u != o))
+				{
+					putchar(i);
+					putchar(o);
+					putchar(u);
+					if (i == '7' && o == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
