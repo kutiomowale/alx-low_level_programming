2e6c26a41e1485dd1a37a10f8055ae4a0168cd67
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-09-13 13:13:43 -0700

    Add 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ * This program prints to the screen
+ * the text "_putchar" followed
+ * by a new line using the _putchar function
+ * defined in the file "_putchar.c"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ab[] = "_putchar";
+	int n = 0;
+
+	while (n < 7)
+	{
+		_putchar(ab[n]);
+		n++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
