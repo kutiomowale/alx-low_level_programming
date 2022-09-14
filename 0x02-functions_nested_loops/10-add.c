6ca8bb5e5ba3629255dc85ec04dd119c528ff1d0
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-09-13 21:28:06 -0700

    Add 10-add.c and update header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - This function adds two integers and returns the result.
+ * @a: Parameter 1 be worked on
+ * @b: Parameter 2 be worked on
+ * Return: prints the sum
+ */
+
+int add(int a, int b)
+{
+	int ab = a + b;
+
+	return (ab);
+}
+
