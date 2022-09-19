7b0bf58b5272a653c839c8b73623ff0f09c12763
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-09-19 14:52:50 -0700

    Add main.h header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+int _strlen(char *s);
+
+void swap_int(int *a, int *b);
+
+int _putchar(char c);
+
+void _puts(char *str);
+
+#endif /* MAIN_H */
