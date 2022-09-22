cfb481335a3c6a0fc1865cbfbaa607e05ee67384
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-09-22 04:10:14 -0700

    Add 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,32 +8,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	i = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+
 		i++;
 	}
 
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-
-	if (i > j)
-	{
-		return (15);
-	}
-
-	else if (i < j)
-	{
-		return (-15);
-	}
-
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
