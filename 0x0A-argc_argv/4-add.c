181b45cf0c15efbd787ae0b01daea1e8e0ba65d9
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-06 11:12:39 -0700

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		i = 1;
 		while (i < argc)
 		{
-			if (atoi(argv[i]) == 0 && *argv[i] != '0')
+			if ((atoi(argv[i]) == 0 && *argv[i] != '0') || atoi(argv[i]) < 0)
 			{
 				printf("Error\n");
 				return (1);
