e94117d465f77558ab61d06c9fa523907f04ea24
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-09 16:49:34 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,13 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * followed by a new line
- * 
+ *
  * Numbers must be separated by a comma, followed by a space
  * Numbers should be printed in order
  * The first printed number should be the number passed to your function
  * The last printed number should be 98
  * You are allowed to use the standard library
- * 
+ *
  * @n: starting number
  */
 
