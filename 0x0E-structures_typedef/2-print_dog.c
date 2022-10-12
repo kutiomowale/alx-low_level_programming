67b793cb9b5508fa7f4bf3122834fb7d56ddb002
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-11 23:45:01 -0700

    Add 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,16 +14,30 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
+		{
 			printf("Name: %s\n", d->name);
+		}
 		else
+		{
 			printf("Name: %s\n", nil);
+		}
+		
 		if (d->age)
+		{
 			printf("Age: %.6f\n", d->age);
+		}
 		else
+		{
 			printf("Age: %s\n", nil);
+		}
+		
 		if (d->owner)
+		{
 			printf("Owner: %s\n", d->owner);
+		}
 		else
+		{
 			printf("Owner: %s\n", nil);
+		}
 	}
 }
