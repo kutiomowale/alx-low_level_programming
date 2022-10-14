c7a023e67083a769618aa580f4c920573c71445a
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-14 11:56:58 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,19 +16,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator == NULL)
-		return;
-
-	for (i = 0; i < n; i++)
+	if (separator)
 	{
-		printf("%d", va_arg(ap, int));
-		if (i < (n - 1))
+		for (i = 0; i < n; i++)
 		{
-			printf("%s", separator);
+			printf("%d", va_arg(ap, int));
+			if (i < (n - 1))
+			{
+				printf("%s", separator);
+			}
 		}
+		printf("\n");
 	}
-
-	printf("\n");
 
 	va_end(ap);
 }
