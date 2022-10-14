ebd1e48625bc1b507b4e30ff4d12cfa83c09c535
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-13 19:12:43 -0700

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	int i, lname, lowner
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
@@ -18,9 +19,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	for (lname = 0; name[lname]; lname++)
+		;
+	for (lowner = 0; owner[owner]; lowner++)
+		;
+	
+	ptr->name = malloc(sizeof(char) * (lname + 1));
+	ptr->owner = malloc(sizeof(char) * (lowner + 1));
+
+	if (!(ptr->name) || !(ptr->owner))
+	{
+		free(ptr->name);
+		free(ptr->owner);
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		ptr->name[i] = name[i];
+	ptr->name[i] = '\0';
+	
+	for (i = 0; i < lowner; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner[i] = '\0';
 
 	return (ptr);
 }
