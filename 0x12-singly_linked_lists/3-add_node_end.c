a2c253d7fdc73915d5e18894895f25f239e5d9e2
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-21 17:38:42 -0700

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,19 +10,28 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *ptr;
 
+	char *new_str;
+
 	ptr = *head;
-
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	new_node->str = strdup(str);
+	new_str = strdup(str);
+	if (!new_str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = new_str;
 	new_node->len = strlen(str);
 	new_node->next = NULL;
-
+	if (!(*head))
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
@@ -30,5 +39,5 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	ptr->next = new_node;
 
-	return (new_node);
+	return (ptr);
 }
