6dfac50b77ed80c6cc4d9a4765f1ece23c9b21a1
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-23 10:41:34 -0700

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,19 @@
-section	.text
-   global _start     ;must be declared for linker (ld)
-	
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
-   int	0x80        ;call kernel
-	
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+section .data
+	hello:     db 'Hello, Holberton',10    ; 'Hello world!' plus a linefeed character
+	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+	                                   ; (I'll explain soon)
 
-section	.data
-msg db 'Hello, Holberton', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
+section .text
+	global _start
+
+_start:
+	mov eax,4            ; The system call for write (sys_write)
+	mov ebx,1            ; File descriptor 1 - standard output
+	mov ecx,hello        ; Put the offset of hello in ecx
+	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
+	                     ;  mov edx,[helloLen] to get it's actual value
+	int 80h              ; Call the kernel
+
+	mov eax,1            ; The system call for exit (sys_exit)
+	mov ebx,0            ; Exit with return code of 0 (no error)
+	int 80h
