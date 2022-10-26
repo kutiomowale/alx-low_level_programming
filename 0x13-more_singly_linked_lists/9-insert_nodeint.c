a32afb81e2985c8f83b30dc142047e922d265d7c
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-25 19:21:01 -0700

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	listint_t *new_node;
 
-	if (head == NULL)
+	if ((head == NULL) || (*head == NULL))
 	{
 		return (NULL);
 	}
