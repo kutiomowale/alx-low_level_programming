bdafca2ef439424ee47f825e1b60f8b8b7222224
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-02-23 08:38:13 -0800

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
@@ -19,5 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 	return (table);
 }
