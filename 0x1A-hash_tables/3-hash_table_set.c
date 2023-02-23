52dcdf67894e0174cdcdf8aaa53856eb0267a867
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-02-23 12:21:44 -0800

    Add 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,85 @@
+#include "hash_tables.h"
+
+/**
+ * create_node - A function that creates a node of a hash table
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key.
+ * value must be duplicated. value can be an empty string
+ *
+ * Return: The node if it succeeded, NULL otherwise
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	if (!key || !value)
+		return (NULL);
+	if (strlen(key) == 0)
+		return (NULL);
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = malloc(strlen(key) + 1);
+	if (!node->key)
+		return (NULL);
+	node->value = malloc(strlen(value) + 1);
+	if (!node->value)
+		return (NULL);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - A function that adds an element to the hash table
+ * @ht: is the hash tabl to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key.
+ * value must be duplicated. value can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, the new node is added at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *new_node, *temp;
+
+	if ((!ht || !key) || (!value))
+		return (0);
+	if (strlen(key) == 0)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			strcpy(node->value, value);
+			return (1);
+		}
+		temp = node->next;
+		while (temp)
+		{
+			if (strcmp((temp->key), key) == 0)
+			{
+				strcpy(temp->value, value);
+				return (1);
+			}
+			temp = temp->next;
+		}
+		new_node = create_node(key, value);
+		if (!new_node)
+			return (0);
+		new_node->next = node;
+		ht->array[index] = new_node;
+		return (1);
+	}
+	node = create_node(key, value);
+	if (!node)
+		return (0);
+	ht->array[index] = node;
+	return (1);
+}
